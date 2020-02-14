0b93889b28d787fd3e0f002728db2ab6893517cd
Claudia Maciel <clc_80@yahoo.com>
2020-02-13 18:34:31 -0800

    Initial Commit

---
+++ Stars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by Claudia Contreras on 2/13/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
